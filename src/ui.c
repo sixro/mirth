9ef90d7787343af60558f7ed141ceb254e9757c5
sixro <sixro@users.noreply.github.com>
2025-08-19 13:48:27 +0200

    feat: drew a rect, circle and a texture

--- src/ui.c
+++ src/ui.c
@@ -2,16 +2,20 @@
 
 #include "raylib.h"
 
-void ui_init() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+const int screenWidth = 800;
+const int screenHeight = 450;
 
+void ui_init() {
     InitWindow(screenWidth, screenHeight, "Mirth");
 
     SetTargetFPS(60);
 }
 
 void ui_loop() {
+	Image character = LoadImage("./asset/mirth_character.png");
+	Texture2D texture = LoadTextureFromImage(character);
+	UnloadImage(character);
+
     while (!WindowShouldClose())
     {
 		/*
@@ -24,10 +28,16 @@ void ui_loop() {
 
             ClearBackground(RAYWHITE);
 
+			DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
+			DrawCircle(screenWidth/5, 120, 35, DARKBLUE);
+			DrawTexture(texture, screenWidth/2 - texture.width/2, screenHeight/2 - texture.height/2 - 40, WHITE);
+
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
     }
+
+	UnloadTexture(texture);
 }
 
 void ui_close() {
